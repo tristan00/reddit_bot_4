43680888ef62cca76c4130418784a1e89669b823
TD\tdelforge <tristandelforge1@gmail.com>
2017-12-18 21:23:25 -0600

    data processing

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 reddit bot 1, naive method similar to localized regression, some success on the simpler subreddits
 reddit bot 3, dnn to clasifiy comments, would attempt to reuse comments, rank them  by classification post the top one, no success
 reddit bot 4, lstm to generate comments, some undetermined regression algorithm to predict success
+
+
+progress
+built comment chains that should be able to be converted to features for training
+
+next step
+look into how to lsit features, having n most common words would be huge, may be able to use word to vec
+will send in word along side with subreddit and pos
--- lstm_model.py
+++ lstm_model.py
@@ -6,6 +6,9 @@ import collections
 import time
 import nltk
 import re
+import sqlite3
+import pandas as pd
+import nltk
 
 learning_rate = 0.001
 training_iters = 100000
@@ -15,6 +18,12 @@ n_hidden = 1024
 max_vocab_size = 50000
 pos_size = 36
 total_vocab_size =  max_vocab_size + pos_size
+db_location = r'C:\Users\tdelforge\Documents\project_dbs\reddit\reddit.db'
+comment_max_len = 100
+paragraph_marker = ' marker_new_paragraph '
+new_comment_marker = ' marker_new_comment '
+tab_marker = ' marker_new_paragraph '
+return_marker = ' marker_return '
 
 def get_model(weights, biases):
     x = tf.placeholder("float", [None, n_input, 1])
@@ -33,3 +42,47 @@ def train_model():
     x = tf.placeholder("float", [None, n_input, 1])
     y = tf.placeholder("float", [None, total_vocab_size])
     model  = get_model(x, weights, biases)
+
+
+def get_input_text_from_comment_chain(comment_chain):
+    formatted_chain = []
+    for i in comment_chain:
+        formatted_chain.append(i.replace(r'\n', paragraph_marker).replace(r'\t', tab_marker).replace(r'\r',return_marker))
+    text_line = new_comment_marker.join(formatted_chain)
+    tokenized_input = nltk.word_tokenize(text_line)
+    print(tokenized_input)
+
+
+def get_data(num_of_comment_roots = 100):
+    with sqlite3.connect(db_location) as conn:
+        # get root comments
+        p_df = pd.read_sql('select * from comments where parent_id is Null', conn)
+
+        comment_chains = []
+        for count, (i, j) in enumerate(p_df.iterrows()):
+            if count > 10:
+                break
+            comment_chain = []
+
+            #get parent title
+            title_text = conn.execute('select title from posts where p_id = ? limit 1', (j['p_id'], )).fetchone()[0]
+            comment_chain.append(title_text)
+
+            #add root comment
+            comment_chain.append(j['body'])
+
+            #for now pick highest score child
+            parent_id = j['c_id']
+            while True:
+                next_child = conn.execute('select body, c_id from comments where parent_id = ? order by score DESC', (parent_id,)).fetchone()
+                if next_child:
+                    reply_text, parent_id = next_child
+                    comment_chain.append(reply_text)
+                else:
+                    break
+            comment_chains.append(comment_chain)
+    for i in comment_chains:
+        get_input_text_from_comment_chain(i)
+
+
+get_data()
